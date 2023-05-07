73d5ff0de239b0b52f1f8720ef646d27ac510d60
Ridham Patel <ridham.patel@bacancy.com>
2023-05-07 10:40:41 +0530

    added account creation function at the time of customer's account request approvation by employee.

--- app/mailers/account_request_mailer.rb
+++ app/mailers/account_request_mailer.rb
@@ -4,4 +4,10 @@ class AccountRequestMailer < ApplicationMailer
 
     mail(to: User.with_role(:employee).pluck(:email), subject: 'New account request')
   end
+
+  def account_created(account_request, account)
+    @account_request = account_request
+    @account = account
+    mail(to: @account_request.user.email, subject: 'Your account has been created')
+  end
 end
